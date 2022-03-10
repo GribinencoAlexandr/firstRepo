2a357c7b37a90f15d01e1493158d3990a1b9294a
GribinencoAlexandr <alexandr.gribinenco@jivygroup.com>
2022-03-10 14:41:23 +0200

    Add info and game rule tabs

--- my-app/src/Services/loginThunks.js
+++ my-app/src/Services/loginThunks.js
@@ -3,6 +3,7 @@ export const getLogin = (url, data) => {
   return axios
     .post(url, data)
     .then((result) => {
+      console.log("login", result);
       return result.data;
     })
     .catch((error) => {
--- my-app/src/store/appData/actions.js
+++ my-app/src/store/appData/actions.js
@@ -3,3 +3,5 @@ import { createAction } from "@reduxjs/toolkit";
 export const loaderAC = createAction("LOADER");
 export const loadingBarAC = createAction("LOADING_BAR");
 export const menuTabAC = createAction("MENU_TAB");
+export const gameRuleTabAC = createAction("GAME_RULE_TAB");
+export const infoTabAC = createAction("INFO_TAB");
